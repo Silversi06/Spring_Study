dee2ba3879c9d31c3ab0de622c1d1218341bfa0b
Silversi06 <w2219@e-mirim.hs.kr>
2024-03-28 10:03:30 +0900

    Spring study 커밋

---
+++ src/main/java/com/example/demo/MyRestController.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+@RestController
+@RequestMapping("/renew")
+public class MyRestController {
+
+    @GetMapping(value = "/hello-html", produces = MediaType.TEXT_HTML_VALUE)
+// 반환 코드도 마찬가지로 그냥 성공적으로 메서드에서 값을 반환하면 자동으로 200이 됨
+    @ResponseStatus(HttpStatus.OK)
+    public String helloHTML() {
+        return "<h1>Hello</h1>";
+    }
+    @GetMapping(value = "/echo-repeat", produces = MediaType.TEXT_PLAIN_VALUE)
+// @RequestHeader 어노테이션을 통해서 X-Repeat-Count에 적힌 숫자 정보 가져오고 없으면 1로 초기화
+    public String echoRepeat(@RequestParam(value = "X-repeat-count",defaultValue = "1") String word, @RequestHeader(value = "X-Repeat-Count", defaultValue = "1") Integer repeatCount) throws IOException {
+        String result = "";
+        for(int i=0;i<repeatCount;i++) {
+            result += word;
+        }
+        return result;
+    }
+
+    @GetMapping(value = "/dog-image", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] dogImage() throws IOException {
+        // resources 폴더의 static 폴더에 이미지 있어야 함
+        File file = ResourceUtils.getFile("classpath:static/dog.jpg");
+        // 파일의 바이트 데이터 모두 읽어오기
+        return Files.readAllBytes(file.toPath());
+    }
+
+
+    @GetMapping(value = "/dog-image-file", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+// 헤더를 직접 조정하고 싶은 경우 ResponseEntity 타입을 반환하도록 설정 가능
+// (꺽쇠 안에는 응답 메시지의 바디 데이터에 포함될 타입을 지정)
+    public ResponseEntity<byte[]> dogImageFile() throws IOException {
+        File file = ResourceUtils.getFile("classpath:static/dog.jpg");
+        byte[] bytes = Files.readAllBytes(file.toPath());
+
+        String filename = "dog.jpg";
+        // 헤더 값 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=" + filename);
+
+        return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
+    }
+
+}
+
+
+

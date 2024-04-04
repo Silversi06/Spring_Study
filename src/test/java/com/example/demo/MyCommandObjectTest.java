1050459bacd240832b4c37bc430cd488db9508e7
Silversi06 <w2219@e-mirim.hs.kr>
2024-04-04 10:27:29 +0900

    @ModelAttribute 어노테이션과 커맨드 객체(Command Object) 활용

---
+++ src/test/java/com/example/demo/MyCommandObjectTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyCommandObjectTest {
+
+    @Test
+    void setValue1() {
+    }
+
+    @Test
+    void setValue2() {
+    }
+}

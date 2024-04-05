16acf15f139d8064a4ce23d530320e2798ca16d8
Silversi06 <w2219@e-mirim.hs.kr>
2024-04-05 15:10:25 +0900

    @ModelAttribute 어노테이션과 커맨드 객체(Command Object) 활용

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -23,6 +23,34 @@ class Test2 {
     @Override public String toString() {return "Test2{result='" + result + '\'' + ", code=" + code + '}'; }
 }
 
+class Test3{
+    private String title;
+    private String author;
+    private Integer first_realease;
+    private Integer rating;
+
+    public String getTitle() {return title;}public void setTitle(String title) {this.title = title;}public String getAuthor() {return author;}public void setAuthor(String author) {this.author = author;}public Integer getFirst_realease() {return first_realease;}public void setFirst_realease(Integer first_realease) {this.first_realease = first_realease;}public Integer getRating() {return rating;}public void setRating(Integer rating) {this.rating = rating;}
+
+    @Override public String toString() {return "Test3{title='" + title + '\'' + ", author='" + author + '\'' + ", first_realease=" + first_realease + ", rating=" + rating + '}'; }
+}
+class Test4 {
+    private String result;
+    private String id;
+    private Boolean success;
+
+    public String getResult() {return result;}public void setResult(String result) {this.result = result;}public String getId() {return id;}public void setId(String id) {this.id = id;}public Boolean getSuccess() {return success;}public void setSuccess(Boolean success) {this.success = success;}
+
+    @Override public String toString() {return "Test4{result='" + result + '\'' + ", id='" + id + '\'' + ", success=" + success + '}'; }
+}
+
+class Test5{
+    private String text;
+    private Integer count;
+
+    public String getText() {return text;}public void setText(String text) {this.text = text;}public Integer getCount() {return count;}public void setCount(Integer count) {this.count = count;}
+
+    @Override public String toString() {return "Test5{text='" + text + '\'' + ", count=" + count + '}'; }
+}
 
 @RestController
 @RequestMapping("/sung")
@@ -30,12 +58,31 @@ public class TestController {
 
     @GetMapping("/chan")
     @ResponseBody
-    public Test2 Chan(@RequestBody Test test){
+    public Test2 Chan(@RequestBody Test test) {
         System.out.println(test.getMessage());
         Test2 test2 = new Test2();
         test2.setCode(1001);
         test2.setResult("success");
         return test2;
     }
-
+    @GetMapping("/eun")
+    @ResponseBody
+    public Test4 Eun(@RequestBody Test3 test3){
+        System.out.println(test3.getTitle());
+        Test4 test4 = new Test4();
+        test4.setResult("created");
+        test4.setId("a1234");
+        test4.setSuccess(true);
+        return test4;
+    }
+    @GetMapping("/count")
+    public String Count(@RequestBody Test5 test5){
+        String result ="";
+        for(int i = 0; i< test5.getCount(); i++){
+            result+= test5.getText();
+        }
+        return result;
+    }
 }
+
+
